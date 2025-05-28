bddab3fe0b5c6d5d619d0ef892c5c5f961531d20
ybounite <dimayoussef42@gmail.com>
2025-05-28 15:23:35 +0100

    fix nor

--- main.c
+++ main.c
@@ -6,99 +6,95 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:59:33 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/21 17:00:14 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:18:37 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool	ft_isdigit(int c)
-{
-	return ('0' <= c && '9' >= c);
-}
-
-bool	check_isdigit(char *str)
+bool	ft_create_pthread(t_data_philo *t_data)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (i < t_data->number_of_philos)
 	{
-		if (i == 0 && (str[0] == '+'|| str[0] == '-'))
-			i++;
-		if (!ft_isdigit(str[i]))
-			return (syntax_error(2), true);
+		if (pthread_create(&t_data->philos[i].thread,
+				NULL, &philo_routine, &t_data->philos[i]) != 0)
+		{
+			free(t_data->forks);
+			free(t_data->philos);
+			printf("FILED TO CREATE IN THREAD\n");
+			return (false);
+		}
 		i++;
-	}
-	if (i == false)
-		return (syntax_error(2), true);
-	return (false);
-}
-
-bool	ft_isemtystr(char *str)
-{
-	return (*str == '\0');
-}
-
-bool	check_syntax_error(int arc, char **arv)
-{
-	int	i;
-
-	i = 1;
-	while (i < arc)
-	{
-		if (check_isdigit(arv[i]) || ft_isemtystr(arv[i]))
-			return (true);
-		i++;	
-	}
-	return (false);
-}
-
-void	data_init(t_data_philo *t_data, int arc, char **arv)
-{
-	t_data->number_of_philos = ft_atoi(arv[1]);
-	t_data->time_to_die = ft_atoi(arv[2]);
-	t_data->time_to_eat = ft_atoi(arv[3]);
-	t_data->time_to_sleep = ft_atoi(arv[4]);
-	t_data->number_of_times_each_philos_must_eat = -1;
-	if (arc == 6)
-		t_data->number_of_times_each_philos_must_eat = ft_atoi(arv[5]);
-	if (t_data->number_of_philos < 0 || t_data->time_to_die < 0
-		|| t_data->time_to_eat < 0 || t_data->time_to_sleep < 0)
-	{
-		syntax_error(2);
-		t_data->is_error = true;
-	}
-}
-
-bool	parsing(t_data_philo *t_data, int arc, char **arv)
-{
-	if (check_syntax_error(arc, arv))
-		return (t_data->is_error = true, false);
-	data_init(t_data, arc,arv);
-	if (t_data->number_of_philos == 0)
-	{
-		t_data->is_error = true;
-		syntax_error(3);
 	}
 	return (true);
 }
 
+bool	ft_joind_pthread(t_data_philo *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->number_of_philos)
+	{
+		if (pthread_join(data->philos[i].thread, NULL) != 0)
+		{
+			free(data->forks);
+			free(data->philos);
+			printf("FILED TO JOIND THREAD\n");
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+void	ft_destroy(t_data_philo *data)
+{
+	short	i;
+
+	i = 0;
+	while (i < data->number_of_philos)
+		pthread_mutex_destroy(&data->forks[i++]);
+	// pthread_mutex_destroy(&data->write_lock);
+	free(data->forks);
+	free(data->philos);
+}
+
+bool	init_mutex(t_data_philo *data, pthread_t *monitor)
+{
+	pthread_mutex_init(&data->write_lock, NULL);
+	pthread_mutex_init(&data->time, NULL);
+	pthread_mutex_init(&data->death_mutex, NULL);
+	pthread_mutex_init(&data->times_eaten_mutex, NULL);
+	if (pthread_create(monitor, NULL, monitor_death, &data) != 0)
+		return (printf("FAILED TO CREATE MONITOR THREAD\n"), true);
+}
+
 int	main(int arc, char **arv)
 {
-	t_data_philo	t_data;
+	t_data_philo	data;
+	pthread_t		monitor;
 
-	memset(&t_data, false, sizeof(t_data));
+	memset(&data, false, sizeof(data));
 	if (arc != 5 && arc != 6)
-		return (syntax_error(1), true);
-	parsing(&t_data, arc, arv);
-	printf("is error : %d\n", t_data.is_error);
-	if (t_data.is_error)
-		return (true);
-	printf ("number of philosophers\t\t\t : %d\n", t_data.number_of_philos);
-	printf("time to die\t\t\t\t : %d\n", t_data.time_to_die);
-	printf("time to eat\t\t\t\t : %d\n", t_data.time_to_eat);
-	printf("time to sleep\t\t\t\t : %d\n", t_data.time_to_sleep);
-	printf("number of times each philosopher must eat: %d\n", t_data.number_of_times_each_philos_must_eat);
-	return (false);
+		return (syntax_error(1), EXIT_FAILURE);
+	parsing(&data, arc, arv);
+	if (data.is_error)
+		return (EXIT_FAILURE);
+	if (data.num_of_must_eat == 0)
+		return (printf(PHILO_DONE_MSG), EXIT_SUCCESS);
+	if (init_data_philo(&data))
+		return (EXIT_FAILURE);
+	// init_mutex(&data, &monitor);
+	if (!ft_create_pthread(&data))
+		return (EXIT_FAILURE);
+	if (pthread_join(monitor, NULL) != 0)
+		return (printf("FILED TO JOIND MONITOR\n"), EXIT_FAILURE);
+	if (!ft_joind_pthread(&data))
+		return (EXIT_FAILURE);
+	ft_destroy(&data);
+	return (EXIT_SUCCESS);
 }
