c4c7e7b05c12472e0070d01d5f60452a6cadf163
ybounite <dimayoussef42@gmail.com>
2025-05-29 15:00:51 +0100

    initialization of mutex

--- init_data.c
+++ init_data.c
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 14:41:51 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/28 15:31:19 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/29 14:58:33 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool	init_forks(t_data_philo *data)
 	int	i;
 
 	i = 0;
-	data->forks = malloc(sizeof(t_data_philo) * data->num_philo);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philo);
 	if (!data->forks)
 		return (printf("FILED IN MALLOC\n"), true);
 	while (i < data->num_philo)
@@ -38,7 +38,7 @@ bool	init_philosophers(t_data_philo *data)
 	int	i;
 
 	i = 0;
-	data->philos = malloc(sizeof(t_data_philo) * data->num_philo);
+	data->philos = malloc(sizeof(t_philosopher) * data->num_philo);
 	if (!data->philos)
 	{
 		free(data->forks);
@@ -50,7 +50,7 @@ bool	init_philosophers(t_data_philo *data)
 	{
 		data->philos[i].id = i + 1;
 		data->philos[i].last_meal = data->start_time;
-		data->philos[i].times_eaten = 0;
+		data->philos[i].meals_eaten = 0;
 		data->philos[i].left_fork = &data->forks[i];
 		data->philos[i].right_fork = &data->forks[(i + 1) % data->num_philo];
 		data->philos[i].data = data;
@@ -59,11 +59,26 @@ bool	init_philosophers(t_data_philo *data)
 	return (false);
 }
 
+bool	init_mutex(t_data_philo *data)
+{
+	if (pthread_mutex_init(&data->write_lock, NULL) != 0)
+		return (printf("FILED TO INIT MUTEX\n"), true);
+	if (pthread_mutex_init(&data->meal_lock, NULL) != 0)
+		return (printf("FILED TO INIT MUTEX\n"), true);
+	if (pthread_mutex_init(&data->death_lock, NULL) != 0)
+		return (printf("FILED TO INIT MUTEX\n"), true);
+	if (pthread_mutex_init(&data->times_eaten_lock, NULL) != 0)
+		return (printf("FILED TO INIT MUTEX\n"), true);
+	return (false);
+}
+
 bool	init_data_philo(t_data_philo *data)
 {
 	if (init_forks(data))
 		return (EXIT_FAILURE);
 	if (init_philosophers(data))
+		return (EXIT_FAILURE);
+	if (init_mutex(data))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
