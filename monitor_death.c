4be3fad06ce5695943f1aee944b41390ca33122c
ybounite <dimayoussef42@gmail.com>
2025-05-29 15:31:08 +0100

    fix in data race

--- monitor_death.c
+++ monitor_death.c
@@ -6,11 +6,28 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 13:05:57 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/28 15:31:33 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/29 14:52:07 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	print_new_time_to_died(t_data_philo *data)
+{
+	printf("%lld  %d died\n", get_time() - data->start_time, data->philos->id);
+}
+
+void	handle_philosopher_death(t_data_philo *data)
+{
+	pthread_mutex_lock(&data->death_lock);
+	if (!data->someone_died)
+	{
+		print_new_time_to_died(data);
+		data->someone_died = true;
+	}
+	pthread_mutex_unlock(&data->meal_lock);
+	pthread_mutex_unlock(&data->death_lock);
+}
 
 void	*monitor_death(void	*arg)
 {
@@ -18,22 +35,19 @@ void	*monitor_death(void	*arg)
 	int				i;
 
 	data = (t_data_philo *)arg;
+	printf("===> monitor number of ohilo %d <===\n", data->num_philo);
 	while (!data->someone_died)
 	{
 		i = 0;
 		while (i < data->num_philo)
 		{
+			pthread_mutex_lock(&data->meal_lock);
 			if (get_time() - data->philos[i].last_meal > data->time_to_die)
 			{
-				pthread_mutex_lock(&data->death_mutex);
-				if (!data->someone_died)
-				{
-					printf("%lld  %d died\n", get_time() - data->start_time, data->philos->id);
-					data->someone_died = true;
-				}
-				pthread_mutex_unlock(&data->death_mutex);
+				handle_philosopher_death(data);
 				return (NULL);
 			}
+			pthread_mutex_unlock(&data->meal_lock);
 			i++;
 		}
 		usleep(1000);
