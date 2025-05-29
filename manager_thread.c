704968b1ab50b5797eb602836efe727fa8cb9c30
ybounite <dimayoussef42@gmail.com>
2025-05-29 15:34:17 +0100

    create in file of create in thread and joind and destroy data

---
+++ manager_thread.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manager_thread.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 15:09:00 by ybounite          #+#    #+#             */
+/*   Updated: 2025/05/29 15:33:02 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+bool	ft_create_pthread(t_data_philo *t_data, pthread_t *monitor)
+{
+	int	i;
+
+	i = 0;
+	while (i < t_data->num_philo)
+	{
+		if (pthread_create(&t_data->philos[i].thread,
+				NULL, &philo_routine, &t_data->philos[i]) != 0)
+		{
+			free(t_data->forks);
+			free(t_data->philos);
+			printf("FILED TO CREATE IN THREAD\n");
+			return (false);
+		}
+		i++;
+	}
+	if (pthread_create(monitor, NULL, monitor_death, t_data) != 0)
+		return (printf("FAILED TO CREATE MONITOR THREAD\n"), false);
+	return (true);
+}
+
+bool	ft_joind_pthread(t_data_philo *data, pthread_t monitor)
+{
+	int	i;
+
+	i = 0;
+	if (pthread_join(monitor, NULL) != 0)
+		return (printf("FILED TO JOIND MONITOR\n"), false);
+	while (i < data->num_philo)
+	{
+		if (pthread_join(data->philos[i].thread, NULL) != 0)
+		{
+			free(data->forks);
+			free(data->philos);
+			printf("FILED TO JOIND THREAD\n");
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+void	destroy_simulation_data(t_data_philo *data)
+{
+	short	i;
+
+	i = 0;
+	while (i < data->num_philo)
+		pthread_mutex_destroy(&data->forks[i++]);
+	pthread_mutex_destroy(&data->write_lock);
+	pthread_mutex_destroy(&data->meal_lock);
+	pthread_mutex_destroy(&data->death_lock);
+	free(data->forks);
+	free(data->philos);
+}
