693dc4a2d69c73e73b040c2b9f95c57578176c8f
ybounite <dimayoussef42@gmail.com>
2025-05-29 15:37:13 +0100

    fix nor

--- main.c
+++ main.c
@@ -6,78 +6,16 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:59:33 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/28 15:31:19 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/29 15:16:13 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool	ft_create_pthread(t_data_philo *t_data)
-{
-	int	i;
-
-	i = 0;
-	while (i < t_data->num_philo)
-	{
-		if (pthread_create(&t_data->philos[i].thread,
-				NULL, &philo_routine, &t_data->philos[i]) != 0)
-		{
-			free(t_data->forks);
-			free(t_data->philos);
-			printf("FILED TO CREATE IN THREAD\n");
-			return (false);
-		}
-		i++;
-	}
-	return (true);
-}
-
-bool	ft_joind_pthread(t_data_philo *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->num_philo)
-	{
-		if (pthread_join(data->philos[i].thread, NULL) != 0)
-		{
-			free(data->forks);
-			free(data->philos);
-			printf("FILED TO JOIND THREAD\n");
-			return (false);
-		}
-		i++;
-	}
-	return (true);
-}
-
-void	ft_destroy(t_data_philo *data)
-{
-	short	i;
-
-	i = 0;
-	while (i < data->num_philo)
-		pthread_mutex_destroy(&data->forks[i++]);
-	// pthread_mutex_destroy(&data->write_lock);
-	free(data->forks);
-	free(data->philos);
-}
-
-bool	init_mutex(t_data_philo *data, pthread_t *monitor)
-{
-	pthread_mutex_init(&data->write_lock, NULL);
-	pthread_mutex_init(&data->time, NULL);
-	pthread_mutex_init(&data->death_mutex, NULL);
-	pthread_mutex_init(&data->times_eaten_mutex, NULL);
-	if (pthread_create(monitor, NULL, monitor_death, &data) != 0)
-		return (printf("FAILED TO CREATE MONITOR THREAD\n"), true);
-	return (false);
-}
-
 int	main(int arc, char **arv)
 {
 	t_data_philo	data;
-	pthread_t		monitor = 0;
+	pthread_t		monitor;
 
 	memset(&data, false, sizeof(data));
 	if (arc != 5 && arc != 6)
@@ -85,17 +23,14 @@ int	main(int arc, char **arv)
 	parsing(&data, arc, arv);
 	if (data.is_error)
 		return (EXIT_FAILURE);
-	if (data.num_of_must_eat == 0)
+	if (data.num_of_must_eat == false)
 		return (printf(PHILO_DONE_MSG), EXIT_SUCCESS);
 	if (init_data_philo(&data))
 		return (EXIT_FAILURE);
-	// init_mutex(&data, &monitor);
-	if (!ft_create_pthread(&data))
+	if (!ft_create_pthread(&data, &monitor))
 		return (EXIT_FAILURE);
-	if (pthread_join(monitor, NULL) != 0)
-		return (printf("FILED TO JOIND MONITOR\n"), EXIT_FAILURE);
-	if (!ft_joind_pthread(&data))
+	if (!ft_joind_pthread(&data, monitor))
 		return (EXIT_FAILURE);
-	ft_destroy(&data);
+	destroy_simulation_data(&data);
 	return (EXIT_SUCCESS);
 }
