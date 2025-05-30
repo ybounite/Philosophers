df43bdb0d288bc890055cbd7c12998861446f5bb
ybounite <dimayoussef42@gmail.com>
2025-05-30 15:00:48 +0100

    fix routine philosopher

--- philo_routine.c
+++ philo_routine.c
@@ -6,33 +6,33 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 13:02:44 by ybounite          #+#    #+#             */
-/*   Updated: 2025/05/28 15:21:00 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/05/30 09:19:19 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	check_death(t_philosopher *philo)
-{
-	pthread_mutex_lock(&philo->data->death_mutex);
-	if (philo->data->someone_died)
-	{
-		pthread_mutex_unlock(&philo->data->death_mutex);
-		return (1);
-	}
-	pthread_mutex_unlock(&philo->data->death_mutex);
-	return (0);
-}
-
 void	print_action(t_philosopher *philo, const char *action)
 {
-	pthread_mutex_lock(&philo->data->write_lock);
+	pthread_mutex_lock(&philo->data->death_lock);
 	if (!philo->data->someone_died)
 	{
 		printf("%lld %d %s\n", get_time() - philo->data->start_time,
 			philo->id, action);
 	}
-	pthread_mutex_unlock(&philo->data->write_lock);
+	pthread_mutex_unlock(&philo->data->death_lock);
+}
+
+void	think_philo(t_philosopher *philo)
+{
+	print_action(philo, "is thinking");
+}
+
+void	sleep_philo(t_philosopher *philo)
+{
+	print_action(philo, "is sleeping");
+	safe_usleep(philo, philo->data->time_to_sleep);
+
 }
 
 void	*philo_routine(void *arg)
@@ -40,38 +40,15 @@ void	*philo_routine(void *arg)
 	t_philosopher	*philo;
 
 	philo = (t_philosopher *)arg;
-	while (1)
+	while (true)
 	{
 		if (check_death(philo))
-			break ;
-		if (philo->id % 2 != 0) // if ID in odd usleep
-		{
-			printf("thread ID is usleep : %d\n", philo->id);
-			usleep(1000);
-		}
+			return (NULL);
+		think_philo(philo);
+		manager_eating(philo);
 		if (check_death(philo))
-			break ;
-		pthread_mutex_lock(philo->left_fork);
-		print_action(philo, "has taken a fork");
-		pthread_mutex_lock(philo->right_fork);
-		print_action(philo, "has taken a fork");
-		print_action(philo, "is eating");
-		pthread_mutex_lock(&philo->data->time);
-		philo->last_meal = get_time(); // protect
-		pthread_mutex_unlock(&philo->data->time);
-		usleep(philo->data->time_to_eat * 1000); // protect
-		pthread_mutex_lock(&philo->data->times_eaten_mutex);
-		philo->times_eaten++;
-		pthread_mutex_lock(&philo->data->times_eaten_mutex);
-		pthread_mutex_unlock(philo->left_fork);
-		pthread_mutex_unlock(philo->right_fork);
-		if (check_death(philo))
-			break ;
-		print_action(philo, "is thinking");
-		if (check_death(philo))
-			break ;
-		print_action(philo, "is sleeping");
-		usleep(philo->data->time_to_sleep * 1000); // protect
+			return (NULL);
+		sleep_philo(philo);
 	}
 	return (NULL);
 }
