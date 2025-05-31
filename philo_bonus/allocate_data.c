87b2191c86736c2dc44f121b916ba2eb09d2d44a
ybounite <dimayoussef42@gmail.com>
2025-05-31 14:40:47 +0100

    add in ft_malloc

---
+++ philo_bonus/allocate_data.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   allocate_data.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 13:07:38 by ybounite          #+#    #+#             */
+/*   Updated: 2025/05/31 10:55:11 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/allocation_manager.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*curr;
+
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	curr = *lst;
+	while (curr->next)
+		curr = curr->next;
+	curr->next = new;
+}
+
+void	ft_lstclear(t_list **list, void (*del)(void *))
+{
+	t_list	*curr;
+
+	if (!list || !*list || !del)
+		return ;
+	curr = *list;
+	while (curr)
+	{
+		*list = curr;
+		del((*list)->content);
+		curr = curr->next;
+		free(*list);
+	}
+	*list = NULL;
+}
+
+void	memory_released(t_list **head)
+{
+	ft_lstclear(head, free);
+}
+
+void	*ft_malloc(size_t size, short option)
+{
+	static t_list	*head;
+	t_list			*new;
+	void			*ptr;
+
+	if (option == 1)
+	{
+		ptr = malloc(size);
+		if (!ptr)
+			return (memory_released(&head), exit(EXIT_FAILURE), NULL);
+		new = ft_lstnew(ptr);
+		if (!new)
+			return (memory_released(&head), exit(EXIT_FAILURE), NULL);
+		ft_lstadd_back(&head, new);
+		return (ptr);
+	}
+	if (option == 0)
+		memory_released(&head);
+	return (NULL);
+}
