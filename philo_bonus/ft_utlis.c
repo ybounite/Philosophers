4dd033cc5de5f1d98dca192597a7f18afcfd3dee
ybounite <dimayoussef42@gmail.com>
2025-05-31 14:41:21 +0100

    adding ft_utlis.c

---
+++ philo_bonus/ft_utlis.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utlis.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 19:53:33 by ybounite          #+#    #+#             */
+/*   Updated: 2025/05/30 19:11:37 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/philo_bonus.h"
+
+bool	ft_isdigit(int c)
+{
+	return ('0' <= c && '9' >= c);
+}
+
+bool	ft_isemtystr(char *str)
+{
+	return (*str == '\0');
+}
+
+int	ft_atoi(char *str)
+{
+	int (i), (r);
+	long (nbr);
+	(1) && (i = 0), (r = 1), (nbr = 0);
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			return (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - 48);
+		if ((nbr > 2147483647))
+			return (-1);
+		i++;
+	}
+	return (nbr * r);
+}
