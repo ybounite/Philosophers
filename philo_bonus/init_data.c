7e34fbdf31f0e00e73d105b3c90d0aa794dbba7d
ybounite <dimayoussef42@gmail.com>
2025-06-01 15:49:15 +0100

    init in data bonus

---
+++ philo_bonus/init_data.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 11:38:23 by ybounite          #+#    #+#             */
+/*   Updated: 2025/06/01 15:48:55 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/philo_bonus.h"
+
+bool	init_semaphores(t_data_ *data)
+{
+	sem_unlink("sem_forks");
+	sem_unlink("sem_print");
+	sem_unlink("sem_meals");
+	sem_unlink("sem_dead");
+	data->sm_forks = sem_open("sem_forks", O_CREAT, 0644, data->num_philo);
+	if (data->sm_forks == SEM_FAILED)
+		return (ft_putstre("Error creating forks semaphore\n"), false);
+	data->sm_print = sem_open("sem_print", O_CREAT, 0644, 1);
+	if (data->sm_print == SEM_FAILED)
+		return (ft_putstre("Error creating print semaphore\n"), false);
+	data->sm_meals = sem_open("sem_meals", O_CREAT, 0644, 1);
+	if (data->sm_meals == SEM_FAILED)
+		return (ft_putstre("Error creating ms_meals\n"), false);
+	data->sm_dead = sem_open("sem_dead", O_CREAT, 0644, 1);
+	if (data->sm_dead == SEM_FAILED)
+		return (ft_putstre("Error creating ms_dead\n"), false);
+	return (true);
+}
+
+bool	init_philosophers(t_data_ *data)
+{
+	int (i);
+	i = 0;
+	data->philos = ft_malloc(sizeof(t_philosopher) * data->num_philo, ALLOC);
+	while (i < data->num_philo)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].pid = 0;
+		data->philos[i].last_meal = 0;
+		data->philos[i].meals_eaten = 0;
+		data->philos[i].data = data;
+		i++;
+	}
+	return (false);
+}
+
+void	destroy_close_(t_data_ *data)
+{
+	if (data->sm_forks)
+		sem_close(data->sm_forks);
+	if (data->sm_dead)
+		sem_close(data->sm_dead);
+	if (data->sm_meals)
+		sem_close(data->sm_meals);
+	if (data->sm_print)
+		sem_close(data->sm_print);
+	sem_unlink("sem_forks");
+	sem_unlink("sem_print");
+	sem_unlink("sem_meals");
+	sem_unlink("sem_dead");
+	ft_malloc(CLEAR, CLEAR);
+}
+
+bool	init_simulation_data(t_data_ *data)
+{
+	if (init_philosophers(data))
+		return (EXIT_FAILURE);
+	if (!init_semaphores(data))
+		return (destroy_close_(data), EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
